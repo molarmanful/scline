7f65da6546e0b3aeb9acaa5e863d07d1e6ea06b9
molarmanful <benjinpang@gmail.com>
2023-08-26 01:05:10 -0400

    another lil tweak

--- src/routes/run/[code]/+server.js
+++ src/routes/run/[code]/+server.js
@@ -18,10 +18,14 @@ export const GET = async ({ params: { code } }) => {
 
   return event(async emit => {
     let n = 0
+    let fresh = true
     let ec = async m => emit(MSG(n++, await compress(m)))
 
-    await ec('==>\n\n')
     for await (let data of std) {
+      if (fresh) {
+        await ec('==>\n\n')
+        fresh = false
+      }
       data = stripAnsi(data + '')
       await ec(data)
       n %= 9
