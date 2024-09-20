31c82475ec5d607cbe4ce1d231d76ceb7efee305
molarmanful <benjinpang@gmail.com>
2024-09-20 14:31:33 -0400

    browser-node compat

--- src/lib/ffl.ts
+++ src/lib/ffl.ts
@@ -1,23 +1,26 @@
-import { Buffer } from 'node:buffer'
-import { promisify } from 'node:util'
-
 import * as ffl from 'fflate'
+import { base64ToUint8Array, uint8ArrayToBase64 } from 'uint8array-extras'
 
-const encode = (x: Uint8Array) => Buffer.from(x).toString('base64')
-
-const decode = (x: string) => {
-  const b = Buffer.from(x, 'base64')
-  return new Uint8Array(b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength))
-}
+const promisify = <A, B, C>(
+  f: (
+    a: A,
+    b: B,
+    cb: (e: Error | null, x: C) => void
+  ) => void,
+) =>
+  (...args: [A, B]) =>
+    new Promise<C>((resolve, reject) => {
+      f(...args, (err, res) => err ? reject(err) : resolve(res))
+    })
 
 const fpcomp = promisify<Uint8Array, ffl.AsyncGzipOptions, Uint8Array>(ffl.compress)
 const fpdecomp = promisify<Uint8Array, ffl.AsyncGzipOptions, Uint8Array>(ffl.decompress)
 
 export const toB64 = (x: Uint8Array) =>
-  encode(x).replace(/\+/g, '.').replace(/\//g, '_').replace(/=+/, '')
+  uint8ArrayToBase64(x).replaceAll('+', '.').replaceAll('/', '_').replaceAll('=+', '')
 
 export const fromB64 = (x: string) =>
-  decode(x.replace(/\./g, '+').replace(/_/g, '/'))
+  base64ToUint8Array(x.replaceAll('.', '+').replaceAll('_', '/'))
 
 export const compress = async (a: string) =>
   toB64(await fpcomp(ffl.strToU8(a), { level: 9, mem: 12 }))
@@ -26,11 +29,7 @@ export const decompress = async (a: string) =>
   ffl.strFromU8(await fpdecomp(fromB64(a), {}))
 
 export const perm = async (a: string[], h = '#') => {
-  const xs = await Promise.all(
-    a.map(async x =>
-      x && (await compress(x)),
-    ),
-  )
+  const xs = await Promise.all(a.map(async x => x && (await compress(x))))
   return h + xs.join(h)
 }
 
